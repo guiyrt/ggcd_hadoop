565659fd0c3aee226d7dd9608452f9a7a7821751
daniel <danielregado@gmail.com>
2021-04-05 00:14:16 +0100

    Removed unnecessary imports

--- src/main/java/FileOutputFormatters/JsonOutputFormat.java
+++ src/main/java/FileOutputFormatters/JsonOutputFormat.java
@@ -3,7 +3,6 @@ package FileOutputFormatters;
 import RecordWriters.JsonRecordWriter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
