66c91b846b82fb06b3597564c0c13aa23af05e92
daniel <danielregado@gmail.com>
2021-04-07 21:53:42 +0100

    Minor comment correction

--- src/main/java/Reducers/YearMovieReducer.java
+++ src/main/java/Reducers/YearMovieReducer.java
@@ -21,7 +21,7 @@ public class YearMovieReducer extends Reducer<YearRatingPair, YearMovieData, Voi
     private Schema movieRatingInfo;
 
     /**
-     * Loads schemas
+     * Overwrite setup method to loads schemas
      * @param context Reducer context
      * @throws IOException Related with read operations
      */
